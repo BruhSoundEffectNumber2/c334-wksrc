31f7fcd3a17283f1f2f7e253c5016a9a674a64f4
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2026-04-08 12:14:44 -0700

    Add minimal backend http server

---
+++ backend/src/main.py
@@ -0,0 +1,23 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import socketserver
+
+PORT = 8000
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"Hello, World!")
+
+class Server(HTTPServer):
+     allow_reuse_address = True
+
+
+if __name__ == "__main__":
+    try:
+        with Server(("", PORT), Handler) as httpd:
+                print(f"Serving at port {PORT}")
+                httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("Server stopped.")
