c158a1482805a8bd1dabfa8ebe341ceaab3ae6e3
Stuart G <segoldkamp@icloud.com>
2026-04-14 20:40:52 -0700

    file to string and string to file + test

--- test_backend.py
+++ test_backend.py
@@ -1,4 +1,24 @@
 from backend.main import hello
 
+def file_to_String(file_path):
+    with open(file_path, 'r') as file:
+        text = file.read()
+    return text
+
+
+def string_to_file(string, file_path):
+    with open(file_path, 'w') as file:
+        file.write(string)
+
+def test_file_to_String():
+    test_string = "This is a test string."
+    test_file_path = "test_path.txt"
+    
+    string_to_file(test_string, test_file_path)
+    
+    result = file_to_String(test_file_path)
+    
+    assert result == test_string
+
 def test_main():
     assert hello() == "Hello, World!"
